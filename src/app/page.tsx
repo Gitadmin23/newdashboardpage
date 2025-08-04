e072bff45c248963a670008984080de8f8c976ef
egileekada <egileoniso.ekada@gmail.com>
2025-08-04 04:01:55 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { Flex, Image } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <Flex w={"full"} h={"100vh"} >
-      <Image src="/images/logo.png" />
+    <Flex w={"full"} h={"100vh"} justifyContent={"center"} alignItems={"center"} >
+      <Image src="/images/logo.png" w={"40px"} h={"fit-content"}  />
     </Flex>
   );
 }
