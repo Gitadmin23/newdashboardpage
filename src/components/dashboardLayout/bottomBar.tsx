f557e0d903abbd37bd160f6ebf051d7dbd14cb72
egileekada <egileoniso.ekada@gmail.com>
2025-08-14 15:57:01 +0100

    update on bottom bar

--- src/components/dashboardLayout/bottomBar.tsx
+++ src/components/dashboardLayout/bottomBar.tsx
@@ -3,7 +3,7 @@ import { Button, Flex, Text } from "@chakra-ui/react";
 import { usePathname, useRouter } from "next/navigation";
 import UserImage from "../sharedComponent/userimage";
 import { HomeIcon, Login, UsersIcon } from "../svg";
-import { SidebarWalletIcon, KisokIcon } from "../svg/sidebarIcons";
+import { SidebarWalletIcon, KisokIcon, NotificationIcon } from "../svg/sidebarIcons";
 import { EVENTPAGE_URL, LANDINGPAGE_URL } from "@/services/urls";
 import useGetUser from "@/hooks/useGetUser";
 import ModalLayout from "../sharedComponent/modal_layout";
@@ -38,22 +38,46 @@ export default function BottomBar() {
     }
 
     return (
-        <Flex paddingX='20px' zIndex={"100"} position={"sticky"} bottom={"0px"} alignItems={"center"} justifyContent={'space-evenly'} width='100%' height='70px' bg={mainBackgroundColor} borderTopWidth={1} borderTopColor={borderColor} display={['flex', 'flex', 'flex', 'none', 'none']}>
-            <Flex onClick={() => routeHandler("/dashboard")} cursor={"pointer"} width={'40px'} height='40px' borderBottomLeftRadius={'20px'} borderTopLeftRadius={'20px'} borderBottomRightRadius={'20px'} bg={pathname === "/dashboard" ? 'brand.chasescrollBlue' : secondaryBackgroundColor} color={pathname === "/dashboard" ? 'white' : bodyTextColor} justifyContent={'center'} alignItems={'center'}>
-                <HomeIcon />
+        <Flex paddingX='8px' zIndex={"100"} position={"sticky"} bottom={"0px"} alignItems={"center"} justifyContent={'space-evenly'} width='100%' height='70px' bg={mainBackgroundColor} borderTopWidth={1} borderTopColor={borderColor} display={['flex', 'flex', 'flex', 'none', 'none']}>
+            <Flex onClick={() => routeHandler("/dashboard")} cursor={"pointer"} flexDir={"column"} width={"fit-content"} color={pathname === "/dashboard" ? primaryColor : bodyTextColor} justifyContent={'center'} alignItems={'center'}>
+                <Flex justifyContent={"center"} alignItems={"center"} h={"34px"} >
+                    <HomeIcon />
+                </Flex>
+                <Text fontSize={"10px"} fontWeight={"medium"} >Home</Text>
             </Flex>
-            <Flex onClick={() => routeHandler("/dashboard/settings/payment/details")} cursor={"pointer"} width={'40px'} height='40px' borderBottomLeftRadius={'20px'} borderTopLeftRadius={'20px'} borderBottomRightRadius={'20px'} bg={pathname === "/dashboard/settings/payment/details" ? 'brand.chasescrollBlue' : secondaryBackgroundColor} color={pathname?.includes('explore') ? 'white' : bodyTextColor} justifyContent={'center'} alignItems={'center'}>
-                <SidebarWalletIcon mobile={true} size={"20px"} color={pathname === "/dashboard/settings/payment/details" ? true : false} />
+            <Flex onClick={() => routeHandler("/dashboard/settings/payment/details")} flexDir={"column"} cursor={"pointer"} width={"fit-content"} color={pathname?.includes('explore') ? primaryColor : bodyTextColor} justifyContent={'center'} alignItems={'center'}>
+                <Flex justifyContent={"center"} alignItems={"center"} h={"34px"} >
+                    <SidebarWalletIcon mobile={true} size={"20px"} color={pathname === "/dashboard/settings/payment/details" ? true : false} />
+                </Flex>
+                <Text fontSize={"10px"} fontWeight={"medium"} >Wallet</Text>
             </Flex>
-            <Flex onClick={() => routeHandler('/product/events')} cursor={"pointer"} width={'40px'} height='40px' borderBottomLeftRadius={'20px'} borderTopLeftRadius={'20px'} borderBottomRightRadius={'20px'} bg={(pathname?.includes('product') || pathname?.includes('kiosk') || pathname?.includes('donation') || pathname?.includes('event')) ? secondaryBackgroundColor : secondaryBackgroundColor} color={pathname?.includes('explore') ? 'white' : bodyTextColor} justifyContent={'center'} alignItems={'center'}>
-                <KisokIcon size='20px' color={(pathname?.includes('product') || pathname?.includes('kiosk') || pathname?.includes('donation') || pathname?.includes('event')) ? true : false} />
+            <Flex onClick={() => routeHandler('/product/events')} cursor={"pointer"} flexDir={"column"} width={"fit-content"} color={(pathname?.includes('product') || pathname?.includes('kiosk') || pathname?.includes('donation') || pathname?.includes('event')) ? primaryColor : bodyTextColor} justifyContent={'center'} alignItems={'center'}>
+                <Flex justifyContent={"center"} alignItems={"center"} h={"34px"} >
+                    <KisokIcon size='20px' color={(pathname?.includes('product') || pathname?.includes('kiosk') || pathname?.includes('donation') || pathname?.includes('event')) ? true : false} />
+                </Flex>
+                <Text fontSize={"10px"} fontWeight={(pathname?.includes('product') || pathname?.includes('kiosk') || pathname?.includes('donation') || pathname?.includes('event')) ? "bold" : "medium"} >Event hub</Text>
             </Flex>
-            <Flex onClick={() => routeHandler('/dashboard/community')} cursor={"pointer"} width={'40px'} height='40px' borderBottomLeftRadius={'20px'} borderTopLeftRadius={'20px'} borderBottomRightRadius={'20px'} bg={pathname?.includes('community') ? 'brand.chasescrollBlue' : secondaryBackgroundColor} color={pathname?.includes('community') ? 'white' : bodyTextColor} justifyContent={'center'} alignItems={'center'}>
-                <UsersIcon />
+            <Flex onClick={() => routeHandler('/dashboard/notification')} cursor={"pointer"} flexDir={"column"} alignItems={"center"} width={"fit-content"} color={pathname?.includes('notification') ? primaryColor : bodyTextColor} justifyContent={'center'}>
+                <Flex justifyContent={"center"} alignItems={"center"} h={"34px"} >
+                    <NotificationIcon color={(pathname?.includes('notification') ? true : false)} size="20px" />
+                </Flex>
+                <Text fontSize={"10px"} fontWeight={"medium"} >Notification</Text>
             </Flex>
-            <Flex onClick={() => routeHandler(`/dashboard/profile/${user?.userId}`)} cursor={"pointer"} >
-                <UserImage size={"40px"} border={"1px"} font={"16px"} data={user} image={user?.data?.imgMain?.value} />
+            <Flex onClick={() => routeHandler('/dashboard/community')} cursor={"pointer"} flexDir={"column"} alignItems={"center"} width={"fit-content"} color={pathname?.includes('community') ? primaryColor : bodyTextColor} justifyContent={'center'}>
+                <Flex justifyContent={"center"} alignItems={"center"} h={"34px"} >
+                    <UsersIcon />
+                </Flex>
+                <Text fontSize={"10px"} fontWeight={"medium"} >Community</Text>
             </Flex>
+            <Flex onClick={() => routeHandler(`/dashboard/profile/${user?.userId}`)} flexDir={"column"} alignItems={"center"} cursor={"pointer"} >
+                <Flex justifyContent={"center"} alignItems={"center"} h={"34px"} >
+                <UserImage size={"30px"} border={"1px"} font={"16px"} data={user} image={user?.data?.imgMain?.value} />
+                </Flex>
+                <Text fontSize={"10px"} color={pathname?.includes("profile") ? primaryColor : bodyTextColor} fontWeight={"medium"} >Profile</Text>
+            </Flex>
+            {/* <Flex onClick={() => routeHandler(`/dashboard/profile/${user?.userId}`)} cursor={"pointer"} >
+                <UserImage size={"30px"} border={"1px"} font={"16px"} data={user} image={user?.data?.imgMain?.value} />
+            </Flex> */}
 
             <ModalLayout size={"sm"} open={show} close={()=> console.log("logout")} >
                 <Flex
